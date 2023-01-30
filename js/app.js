58bd0ae7cad6355243f3ca1601efb54263318140
MarianoLMR <mmlr.mariano@gmail.com>
2023-01-30 19:56:21 -0300

    Sección experiencia y contacto

--- js/app.js
+++ js/app.js
@@ -8,3 +8,35 @@ function toggleMenu() {
 
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
+
+const menuLinks = document.querySelectorAll('.nav_menu a[href^="#"]')
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        entry.target.getAttribute("id");
+        document.querySelector('.nav_menu a[href="#${id}"]')
+
+        if (entry.isIntersecting) {
+            document.querySelector("nav_menu a.selected").classList.remove("selected");
+            menuLink.classList.add("selected");
+        } else {
+            menuLink.classList.remove("selected");
+        }
+
+    });
+}, {rootMargin: "-30% 0px -70% 0px"});
+
+menuLinks.forEach(menuLink => {
+    menuLink.addEventListener("click", function() {
+        menu.classList.remove("menu_opened");
+    })
+
+      const hash  = menuLink.getAttribute("href");
+      const target  = document.querySelector(hash);
+      if(target) {
+        observer.observe(target)
+      }
+})
+
+
+
